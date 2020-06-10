fc328fb8c708f6a416f97b91229c860c9d66dd3a
raad <raam.balaji.d@oracle.com>
2020-06-10 21:07:47 +0530

    adding port env to express application

--- api/app.js
+++ api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+
+/*
+    Heroku dynamically assigns a port to the application
+    through this env varaible
+*/
+const port = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
