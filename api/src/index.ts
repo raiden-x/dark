381de53cde70290573264f023aa57295f390a979
raad <raam.balaji.d@oracle.com>
2020-06-13 00:27:28 +0530

    adding dev dep

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './database/createConnection';
-/*
-    Heroku dynamically assigns a port to the application
-    through this env varaible
-*/
-console.log(dotenv.config());
+
+dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
-
 app.get('/', (req, res) => {
   console.log(req);
   res.send('Hello World!');
