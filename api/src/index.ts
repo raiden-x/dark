ac51d3710bd7c90d817e128b0e11b936868eae23
raad <raam.balaji.d@oracle.com>
2020-06-13 02:14:46 +0530

    adding client build

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './database/createConnection';
+import path from 'path';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
-app.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello World!');
-});
+const staticPath = path.resolve(__dirname, '../../client/dist');
+console.log(staticPath);
+app.use(express.static(staticPath));
 
 app.listen(port, () => console.log(`example app started at port ${port}`));
 connectToDatabase()
