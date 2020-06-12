11c62d861f33292a7ffcc1a7932bb6dca3c27940
raad <raam.balaji.d@oracle.com>
2020-06-12 23:14:53 +0530

    adding changes

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,21 +1,27 @@
 import express from 'express';
 import { createConnection } from 'typeorm';
+import dotenv from 'dotenv';
 
 /*
     Heroku dynamically assigns a port to the application
     through this env varaible
 */
+console.log(dotenv.config());
 const port = process.env.PORT || 3000;
 const app = express();
 
 async function connectToDb(): Promise<void> {
-  await createConnection({
-    type: 'postgres',
-    database: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  try {
+    await createConnection({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  } catch (err) {
+    console.log(err);
+  }
   console.log('connected to db');
 }
 
